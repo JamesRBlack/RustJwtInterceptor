67c82b3c8693de4e368437cfa0bcbfcfd37ff4e4
James Black <sitjxb4@sonichealth.com.au>
2025-06-11 16:30:55 +1000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use hyper::server::Server;
 use hyper::service::{make_service_fn, service_fn};
 
-mod proxy;
 mod jwt;
+mod proxy;
 mod types;
 
 #[tokio::main]
@@ -11,9 +11,8 @@ async fn main() {
     let addr = ([127, 0, 0, 1], 3000).into();
 
     // Create a service that clones no state, just calls our handler
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, hyper::Error>(service_fn(proxy::handle_request))
-    });
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, hyper::Error>(service_fn(proxy::handle_request)) });
 
     let server = Server::bind(&addr).serve(make_svc);
 
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,37 +1,88 @@
-use hyper::{Body, Client, Request, Response, Uri};
-use hyper::body::to_bytes;
-use hyper::header::{AUTHORIZATION, CONTENT_TYPE};
 use crate::jwt::generate_jwt;
 use dotenvy::dotenv;
+use hyper::body::to_bytes;
+use hyper::header::{AUTHORIZATION, CONTENT_TYPE};
+use hyper::{Body, Client, Request, Response, Uri};
+use std::str;
 
 pub async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     dotenv().ok();
 
+    let (parts, body) = req.into_parts();
+    let method = parts.method.clone();
+    let uri = parts.uri.clone();
+    let headers = parts.headers.clone();
+
+    let whole_body = to_bytes(body).await?;
+    let body_str = String::from_utf8_lossy(&whole_body);
+
     let dest_base = std::env::var("DEST_BASE").expect("DEST_BASE must be set");
-    let path = req.uri().path_and_query().map(|x| x.as_str()).unwrap_or("/").to_string();
+    let path = uri.path_and_query().map(|x| x.as_str()).unwrap_or("/");
     let dest_uri: Uri = format!("{}{}", dest_base, path).parse().unwrap();
-    let method = req.method().clone();
-    let headers = req.headers().clone();
-    let whole_body = to_bytes(req.into_body()).await.unwrap();
+
+    // Generate JWT token to inject
     let jwt = generate_jwt();
-    
 
     let mut builder = Request::builder()
-        .method(method)
-        .uri(dest_uri);
+        .method(method.clone())
+        .uri(dest_uri.clone());
 
     if let Some(hdrs) = builder.headers_mut() {
+        // Set content type (you can customize this)
         hdrs.insert(CONTENT_TYPE, "application/json".parse().unwrap());
-        hdrs.insert(AUTHORIZATION, format!("Bearer {}", jwt).parse().unwrap());
+
+        // Inject JWT in custom header X-Proxy-Authorization
+        hdrs.insert(
+            "X-Proxy-Authorization",
+            format!("Bearer {}", jwt).parse().unwrap(),
+        );
+
+        // Forward all other headers except the original Authorization and Content-Type
         for (k, v) in headers.iter() {
             if k != AUTHORIZATION && k != CONTENT_TYPE {
                 hdrs.insert(k, v.clone());
             }
         }
     }
-    
-    let proxied = builder.body(Body::from(whole_body)).unwrap();
-    let client = Client::new();
 
-    client.request(proxied).await
+    println!("--- Incoming Proxy Request ---");
+    println!("Method: {}", method);
+    println!("URI: {}", uri);
+    println!("Headers:");
+    for (k, v) in headers.iter() {
+        if k == AUTHORIZATION {
+            println!("  {}: <redacted>", k);
+        } else {
+            println!("  {}: {:?}", k, v);
+        }
+    }
+    println!("Body: {}", body_str);
+
+    if let Some(hdrs) = builder.headers_ref() {
+        println!("--- Outgoing Proxied Request Headers ---");
+        for (k, v) in hdrs.iter() {
+            println!("  {}: {:?}", k, v);
+        }
+    }
+
+    let proxied = builder.body(Body::from(whole_body)).unwrap();
+    println!("Forwarding request to: {}", dest_uri);
+
+    let client = Client::new();
+    let response = client.request(proxied).await?;
+
+    println!("--- Response from downstream ---");
+    println!("Status: {}", response.status());
+    println!("Headers:");
+    for (k, v) in response.headers().iter() {
+        println!("  {}: {:?}", k, v);
+    }
+
+    let (parts, body) = response.into_parts();
+    let body_bytes = to_bytes(body).await?;
+    let body_str = String::from_utf8_lossy(&body_bytes);
+    println!("Body: {}", body_str);
+
+    let new_response = Response::from_parts(parts, Body::from(body_bytes));
+    Ok(new_response)
 }
